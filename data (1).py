cf525e7329efe4d2c844ec7cf8298c71acbcccb1
SolarX <vihaan.punjabi11@gmail.com>
2024-07-29 22:07:20 -0700

    Update data.py

--- data (1).py
+++ data (1).py
@@ -1,5 +1,28 @@
 import matplotlib.pyplot as plt
 
+import requests
+
+# Define the location coordinates
+latitude = 39.7456
+longitude = -97.0892
+
+# Get the grid points
+response = requests.get(f"https://api.weather.gov/points/{latitude},{longitude}")
+data = response.json()
+grid_id = data['properties']['gridId']
+grid_x = data['properties']['gridX']
+grid_y = data['properties']['gridY']
+
+# Get the forecast
+forecast_url = f"https://api.weather.gov/gridpoints/{grid_id}/{grid_x},{grid_y}/forecast"
+forecast_response = requests.get(forecast_url)
+forecast_data = forecast_response.json()
+
+# Print the forecast
+for period in forecast_data['properties']['periods']:
+    print(f"{period['name']}: {period['detailedForecast']}")
+
+
 x_cords = [1] #add points by making a comma and an integer
 
 y_cords = [1] #add points by making a comma and an integer
